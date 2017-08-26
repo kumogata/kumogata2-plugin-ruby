ccc296f89034f7a6d693fd39edf4ac988358754d
hirocaster <hohtsuka@gmail.com>
2017-08-26 14:38:58 +0900

    Fix error at convert JSON to Ruby
    keys in Hash is frozen, but replace(.gsub!) at convert process
    So, copy keys to string data before gsub!
    
    And, added test it case.

--- spec/kumogata2-plugin-ruby_spec.rb
+++ spec/kumogata2-plugin-ruby_spec.rb
@@ -2,4 +2,19 @@ describe Kumogata2::Plugin::Ruby do
   it do
     expect(1).to eq 1
   end
+
+  describe "#dump" do
+    it "JSON format" do
+
+      data = <<-"JSON"
+      {
+        "Version": "2012-10-17"
+      }
+      JSON
+
+      opts = {}
+      template = Kumogata2::Plugin::Ruby.new(opts).dump(JSON.parse(data))
+      expect(template).to eq "template do\n  Version \"2012-10-17\"\nend\n"
+    end
+  end
 end
